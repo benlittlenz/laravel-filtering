2f61e9ac994f14cc9b2fec7b1ba9f8a3bfd3ddcd
nzbenji <benlittle53@gmail.com>
2020-07-26 12:28:28 +1200

    added Project migrations + factory

---
+++ database/migrations/2020_07_26_000828_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('project_ref');
+            $table->string('project_desc');
+            $table->boolean('completed')->default(false);
+            $table->enum('active', ['Active', 'Inactive'])->default('Active');
+            $table->enum('status', ['New', 'In Progress', 'Pending', 'Completed'])->default('New');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
